55bae062f568c7d5aeb0e81b4fc77149bb56d5dd
Kendra <kdodds@gmail.com>
2015-03-13 15:57:43 -0600

    signup user rspec passing

---
+++ spec/features/signup_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+require "capybara/rails"
+
+feature "SignUp" do
+  scenario "User can register at the signup route" do
+    visit root_path
+    click_on "Sign Up"
+    fill_in "First name", with: "Bill"
+    fill_in "Last name", with: "Murray"
+    fill_in "Email", with: "bill@murray.com"
+    fill_in "Password", with: "password"
+    within ".form-horizontal" do
+      click_on "Sign Up"
+    end
+    expect(page).to have_content("Bill")
+  end
+
+end
