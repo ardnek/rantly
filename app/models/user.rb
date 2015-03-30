c0f0766c0f4885a4868bc60b820a0e9d09ca2f8a
Kendra <kdodds@gmail.com>
2015-03-30 14:13:00 -0600

    added error messages to signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ActiveRecord::Base
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   has_secure_password
+
+  has_many :rants
 
   def full_name
     "#{first_name} #{last_name}"
   end
-
-  has_many :rants
 
 end
