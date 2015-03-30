967fecae5cccb8e40277876147afb7d83be43955
Kendra <kdodds@gmail.com>
2015-03-30 13:51:33 -0600

    validates presence of rant body

--- app/models/rant.rb
+++ app/models/rant.rb
@@ -3,6 +3,7 @@ class Rant < ActiveRecord::Base
 
   validates_length_of :body, :minimum => 144, :allow_blank => :false
   validates :title, presence: true
+  validates :body, presence: true
 
   def self.search(search)
     where("body ILIKE ? or title ILIKE ?", "%#{search}%", "%#{search}%")
